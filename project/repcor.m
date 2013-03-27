8c3f8f6cd934618999dca80084a3c349650d14cd
Luke Robison <lukerobison@gmail.com>
2013-03-26 22:01:24 -0500

    repcor does delay-alignment, dB plots
    should this be 10*log10 or 20*log10?

--- project/repcor.m
+++ project/repcor.m
@@ -12,6 +12,8 @@ function repout = repcor(WFM,REC)
     repout = ifft(rec_spec.*conj(wfm_spec),samps_needed);
     repout = repout(1:samps_good);
     time_ax = 0:(1/REC.fs):REC.duration-1/REC.fs;
-    figure();plot(time_ax,repout);
+    [~,peak_ind] = max(dB10(repout));
+    time_ax = time_ax - time_ax(peak_ind);
+    figure();plot(time_ax,dB10(repout));
     
 end
