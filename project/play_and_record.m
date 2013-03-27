e6867ceb5aa2c490977a61d56319819abd011c0e
Luke Robison <lukerobison@gmail.com>
2013-03-26 21:37:01 -0500

    play_and_record can now repeat waveforms

--- project/play_and_record.m
+++ project/play_and_record.m
@@ -1,8 +1,22 @@
-function REC = play_and_record(input_data)
+function REC = play_and_record(input_data,ntimes,spacing)
 
+    if nargin<2, ntimes=1; end
+    if nargin<3, spacing=0; end
+    
     WFM = standardize_wfm(input_data);
-    p = audioplayer(WFM.data,WFM.fs);
-    r = audiorecorder(WFM.fs,16,1);
+    WFM.fs = 44100;
+
+    zpad = zeros(1,spacing*(1/WFM.fs));
+    wfm_padded = [];
+    for jtimes=1:ntimes
+        wfm_padded = [wfm_padded WFM.data];
+        if(jtimes < ntimes)
+            wfm_padded = [wfm_padded zpad];
+        end
+    end
+    
+    p = audioplayer(wfm_padded,WFM.fs);
+    r = audiorecorder(WFM.fs,24,1);
     record(r);
     pause(2);
     playblocking(p);
