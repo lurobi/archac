5218a113e979bbe37ced7ae4bf0e3b6988ec352f
Luke Robison <lukerobison@gmail.com>
2013-03-26 21:34:55 -0500

    Added .fs field to sweep_generator output

--- project/sweep_generator.m
+++ project/sweep_generator.m
@@ -2,6 +2,7 @@ function WFM = sweep_generator(f1,f2,time_period)
 
     f2x = f1+(f2-f1)/2; % why is this necessary!???
     fs = 44100; % should we oversample perhaps?
+    WFM.fs = fs;
     WFM.nsamp = fs*time_period;
     WFM.time_ax = linspace(0,time_period,WFM.nsamp);
     %fc_vs_time = logspace(log10(f1),log10(f2x),WFM.nsamp);
