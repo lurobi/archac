acd3dd4c432d6beb8791465f5e96207f7f59a304
Luke Robison <lukerobison@gmail.com>
2013-03-26 21:33:36 -0500

    Added repcor

---
+++ project/repcor.m
@@ -0,0 +1,17 @@
+function repout = repcor(WFM,REC)
+
+    WFM = standardize_wfm(WFM);
+    REC = standardize_wfm(REC);
+    
+    % there should probably be some normalization here... not sure what the
+    % correct levels should be.
+    samps_good = max(WFM.nsamp,REC.nsamp);
+    samps_needed = 2*samps_good;
+    wfm_spec = fft(WFM.data,samps_needed);
+    rec_spec = fft(REC.data,samps_needed);
+    repout = ifft(rec_spec.*conj(wfm_spec),samps_needed);
+    repout = repout(1:samps_good);
+    time_ax = 0:(1/REC.fs):REC.duration-1/REC.fs;
+    figure();plot(time_ax,repout);
+    
+end
