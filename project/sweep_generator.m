0f88289893ddb921d5c1e5e282fc14d03bb75308
Luke Robison <lukerobison@gmail.com>
2013-04-04 07:59:39 -0500

    sweep generator uses sin so that LF DC is not a problem

--- project/sweep_generator.m
+++ project/sweep_generator.m
@@ -7,6 +7,6 @@ function WFM = sweep_generator(f1,f2,time_period)
     WFM.time_ax = linspace(0,time_period,WFM.nsamp);
     %fc_vs_time = logspace(log10(f1),log10(f2x),WFM.nsamp);
     fc_vs_time = linspace(f1,f2x,WFM.nsamp);
-    WFM.data = cos(fc_vs_time.*(2.*pi).*WFM.time_ax);
+    WFM.data = sin(fc_vs_time.*(2.*pi).*WFM.time_ax);
     %WFM = standardize_wfm(WFM);
 end
