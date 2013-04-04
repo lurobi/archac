fe3588faa72705fa5334d12933a50db3253f8cc8
Luke Robison <lukerobison@gmail.com>
2013-04-04 08:00:14 -0500

    updates to still-broken white_band_generator

--- project/white_band_generator2.m
+++ project/white_band_generator2.m
@@ -1,4 +1,4 @@
-function WFM = white_band_generator(nsec,lowf,highf,order,fs)
+function WFM = white_band_generator2(nsec,lowf,highf,order,fs)
 % WFM = white_band_generator(nsec,lowf,highf,order,fs)
 %
 % Generates band-limited white noise between lowf (Hz) and highf (Hz) using
@@ -9,7 +9,7 @@ function WFM = white_band_generator(nsec,lowf,highf,order,fs)
     if(nargin<5) fs=44100; end
     
     % set up some parameters
-    overlap=1-0.125;
+    overlap=0.25;
     window = hamming(order,'periodic')';
     window = ones(1,order);
 
@@ -20,25 +20,38 @@ function WFM = white_band_generator(nsec,lowf,highf,order,fs)
     % shifted up to our mid-frequency band.
     midf = (highf+lowf)/2;
     bandwidth = highf-lowf;
-    
-    % you could write a loop which slides some window forward with some
-    % overlap, perform ffts on the white noise, applies filtering in the
-    % frequency domain, and iffts back to make our data, but that would be
-    % slow(ish) in matlab.  Instead arrange our data into matrices and do
-    % it all at once.
-    [~,window_indexes] = gen_overlap_windows(white.data,order,overlap);
-    nchunks = size(window_indexes,1);
-    
     % make the filter
     r = bandwidth/fs;
     t = (1:order) - order/2 - 0.5;
     % the filter has it's own windowing as well.
     B = sinc(r*t).*r.*hamming(order,'periodic')';
+    %B = fir1(order-2,[lowf highf]/(fs/2));
+
+    % you could write a loop which slides some window forward with some
+    % overlap, perform ffts on the white noise, applies filtering in the
+    % frequency domain, and iffts back to make our data, but that would be
+    % slow(ish) in matlab.  Instead arrange our data into matrices and do
+    % it all at once.
+    [~,chunk_indexes] = gen_overlap_windows(white.data,order,overlap);
+    nchunks = size(chunk_indexes,1);
+    WFM.data = zeros(1,chunk_indexes(end));
+    window = hamming(order,'periodic')';
+    for jchunk=1:nchunks
+        wspec = fft(window.* white.data(chunk_indexes(jchunk,:)));
+        heterodyne_ts = cos((chunk_indexes(jchunk,:))/fs * midf*2*pi);
+        bpass_ts = heterodyne_ts .* B;
+        bpass_ts = B;
+        bpass_spec = fft(bpass_ts,order);
+        filt_spec = wspec .* bpass_spec;
+        tmp = real(ifft(filt_spec));
+        plot(chunk_indexes(jchunk,:),tmp);
+        WFM.data(chunk_indexes(jchunk,:)) = tmp;
+    end
     
     % frequency-domain of unfiltered white noise.
     white_spec = specgram(white.data,order,overlap,0,window);
     % the heterodyne used to shift our filter up to our band.
-    heterodyne_ts = sin((window_indexes-1)/fs * midf*2*pi);
+    heterodyne_ts = sin((chunk_indexes-1)/fs * midf*2*pi);
     % time-domain multiply shifts low-pass up to band-pass
     full_filt = heterodyne_ts.*repmat(B,[size(white_spec.data,1),1]);
     % convert to frequency-domain so that we can easily apply filter
@@ -52,7 +65,3 @@ function WFM = white_band_generator(nsec,lowf,highf,order,fs)
     WFM.fs = fs;
     WFM = standardize_wfm(white_band);
 end
-
-%rads_per_samp = midf/fs*2*pi
-%rads_per_period = rads_per_samp*order
-%rads_per_period/(2*pi)
