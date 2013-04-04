b7efe8264194bcba57dee11ccea0c38a4583c8a1
Luke Robison <lukerobison@gmail.com>
2013-04-04 07:58:22 -0500

    fixed a rounding inconsistency in specgram/ispecgram

--- util/ispecgram.m
+++ util/ispecgram.m
@@ -2,7 +2,7 @@ function ts = ispecgram(SG)
     
     num_chunks = size(SG.data,1);
     
-    nsamp_keep = floor(SG.fft_size*(1-SG.overlap));
+    nsamp_keep = round(SG.fft_size*(1-SG.overlap));
     nsamp_tot = num_chunks*nsamp_keep + SG.fft_size-nsamp_keep;
     window = SG.window;
     if(isempty(window))
@@ -11,18 +11,16 @@ function ts = ispecgram(SG)
     
     ts   = zeros(1,nsamp_tot);
     navg = zeros(1,nsamp_tot);
+    iidbg=0;
+    if(iidbg) figure();hold on; end
     for jtime=1:num_chunks
         ts_chunk = real(ifft(SG.data(jtime,:)))./window;
         jstart = (jtime-1)*nsamp_keep+1;
         jend   = jstart+SG.fft_size-1;
         ts(jstart:jend) = ts(jstart:jend) + ts_chunk(1:SG.fft_size);
         navg(jstart:jend) = navg(jstart:jend) + 1;
+        if(iidbg) plot(jstart:jend,ts_chunk); end
     end
     %navg = SG.fft_size/nsamp_keep;
     ts = ts ./ navg;
-    
-    % there is still a scaling error here.  ispecgram(specgram(x)) produces
-    % x but with different amplitude.
-    
-    % possible time drift over long periods as well.
 end
