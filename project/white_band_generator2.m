8aa40d49748bea9431c8693d672f179d4a5e74f7
Luke Robison <lukerobison@gmail.com>
2013-04-07 13:22:01 -0500

    more updates to the still-broken white_band_generator

--- project/white_band_generator2.m
+++ project/white_band_generator2.m
@@ -8,8 +8,11 @@ function WFM = white_band_generator2(nsec,lowf,highf,order,fs)
     if(nargin<4) order=128; end
     if(nargin<5) fs=44100; end
     
+    figure(); hold on; axa = gca;
+    figure(); hold on; axb = gca;
+    
     % set up some parameters
-    overlap=0.25;
+    overlap=0.75;
     window = hamming(order,'periodic')';
     window = ones(1,order);
 
@@ -24,7 +27,7 @@ function WFM = white_band_generator2(nsec,lowf,highf,order,fs)
     r = bandwidth/fs;
     t = (1:order) - order/2 - 0.5;
     % the filter has it's own windowing as well.
-    B = sinc(r*t).*r.*hamming(order,'periodic')';
+    lpass_ts = sinc(r*t).*r; %.*hamming(order,'periodic')';
     %B = fir1(order-2,[lowf highf]/(fs/2));
 
     % you could write a loop which slides some window forward with some
@@ -36,15 +39,26 @@ function WFM = white_band_generator2(nsec,lowf,highf,order,fs)
     nchunks = size(chunk_indexes,1);
     WFM.data = zeros(1,chunk_indexes(end));
     window = hamming(order,'periodic')';
+    cfreqs = linspace(0,fs-1/fs,order) + 1/fs/2; % in Hz
+    periods = (1./cfreqs); % in seconds
+    nnew_pts = round((1-overlap)*order);
+    phase_per_step = (1/fs*nnew_pts) ./ periods .* (2*pi); % in radians
     for jchunk=1:nchunks
         wspec = fft(window.* white.data(chunk_indexes(jchunk,:)));
-        heterodyne_ts = cos((chunk_indexes(jchunk,:))/fs * midf*2*pi);
-        bpass_ts = heterodyne_ts .* B;
-        bpass_ts = B;
+        %heterodyne_ts = cos((chunk_indexes(jchunk,:))/fs * midf*2*pi);
+        heterodyne_ts = cos((1:order)/fs * midf*2*pi);
+        bpass_ts = heterodyne_ts .* lpass_ts;
+        bpass_ts = lpass_ts;
         bpass_spec = fft(bpass_ts,order);
+        % apply the correct phase adjustment to the filter for this time
+        % step.
+        plot(axb,real(ifft(bpass_spec)),'r');
+        bpass_spec = bpass_spec .* exp(1j.*phase_per_step.*jchunk);
+        plot(axb,real(ifft(bpass_spec)));
+        
         filt_spec = wspec .* bpass_spec;
         tmp = real(ifft(filt_spec));
-        plot(chunk_indexes(jchunk,:),tmp);
+        plot(axa,chunk_indexes(jchunk,:),tmp);
         WFM.data(chunk_indexes(jchunk,:)) = tmp;
     end
     
@@ -53,7 +67,7 @@ function WFM = white_band_generator2(nsec,lowf,highf,order,fs)
     % the heterodyne used to shift our filter up to our band.
     heterodyne_ts = sin((chunk_indexes-1)/fs * midf*2*pi);
     % time-domain multiply shifts low-pass up to band-pass
-    full_filt = heterodyne_ts.*repmat(B,[size(white_spec.data,1),1]);
+    full_filt = heterodyne_ts.*repmat(lpass_ts,[size(white_spec.data,1),1]);
     % convert to frequency-domain so that we can easily apply filter
     full_filt_spec = fft(full_filt,order,2);
     % apply filter by freq-domain multiply (<=> time-domain convolve)
